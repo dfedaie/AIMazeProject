c649b9a2bf2759bcd52f4c35220c881999b50fcc
BenECole <benjamin.cole95@gmail.com>
2016-10-27 00:30:29 -0400

    WallHuggerWorking
    the wall hugger actor works now

--- AIProj1.py
+++ AIProj1.py
@@ -46,8 +46,7 @@ def neighbors(graph,current):
     rList=[]
     for x in range (current[0]-1,current[0]+2):
         for y in range (current[1]-1,current[1]+2):
-            if graph[x,y]==0:
-                if x>0 and y>0:
+            if (graph[x,y]==0) and (x>0 and y>0):
                     rList.append(Point(x,y))
     return rList
 def aStarActor(graph, start, goal):
@@ -72,7 +71,58 @@ def aStarActor(graph, start, goal):
                 came_from[next] = current
     
     return came_from, cost_so_far
-
+# 7 0 1    
+# 6   2
+# 5 4 3
+def go(direction,point):
+    if (direction == 7) or (direction == 0) or (direction == 1): #if we move up
+        xVar= -1
+    elif (direction == 2) or (direction == 6): #if we move left or right
+        xVar = 0
+    else: #if we move down
+        xVar = 1
+    if (direction == 5) or (direction == 6) or (direction == 7): #if we move left
+        yVar= -1
+    elif (direction == 4) or (direction == 0): #if we move up or down
+        yVar = 0
+    else: #if we move right
+        yVar = 1
+    moveTo=Point(point[0]+xVar,point[1]+yVar)
+    return moveTo
+def wallHugger(maze):
+    current = Point(0,0)
+    goal = Point(48,48)
+    compass = 0
+    steps = 0
+    while (not ((current[0] == goal[0]) and current[1] == goal[1])) and (steps <10000):
+        backLeft = go((compass-3)%8, current)           
+        backLeftSquare = maze[backLeft[0], backLeft[1]]
+        left = go((compass-2)%8, current)
+        leftSquare = maze[left[0], left[1]]
+        frontLeft = go((compass-1)%8, current)
+        frontLeftSquare = maze[frontLeft[0], frontLeft[1]]
+        
+        if ((backLeftSquare == 0) or (backLeftSquare == 3)) and ((backLeft[0]> 0) and (backLeft[0]<50) and (backLeft[1]> 0) and (backLeft[1]<50)):
+                compass = (compass-3)%8
+                maze[current[0],current[1]]=3
+                current=backLeft
+        elif ((leftSquare == 0) or (leftSquare == 3)) and ((left[0]> 0) and (left[0]<50) and (left[1]> 0) and (left[1]<50)):
+                compass = (compass-2)%8
+                maze[current[0],current[1]]=3
+                current=left        
+        elif ((frontLeftSquare == 0) or (frontLeftSquare == 3)) and ((frontLeft[0]> 0) and (frontLeft[0]<50) and (frontLeft[1]> 0) and (frontLeft[1]<50)):
+                compass = (compass-1)%8
+                maze[current[0],current[1]]=3
+                current=frontLeft
+        elif ((maze[go(compass, current)[0], go(compass, current)[1]] == 0) or (maze[go(compass, current)[0], go(compass, current)[1]] == 3)) and ((go(compass, current)[0]> 0) and (go(compass, current)[0]<50) and (go(compass, current)[1]> 0) and (go(compass, current)[1]<50)):
+                maze[current[0],current[1]]=3
+                current=go(compass, current)
+        else:
+            compass = (compass+1)%8
+        steps +=1
+    pyplot.imshow(maze)
+    pyplot.savefig("wallHugSolution.png")
+    
 
 Dim = 50
 Nstp= 100
@@ -111,11 +161,7 @@ for tt in range (1,Nstp):
                     xx[ii,jj]=0
     maze=xx
     
-for x in range (49):
-    for y in range (49):
-        if maze[x+1,y+1]==1 or maze[x+1,y-1]==1 or maze[x-1,y+1]==1 or maze[x-1,y-1]==1:
-            if random.randint(1,4)<2:
-                maze[x,y]=0
+
                 
 for x in range (50):
     for y in range (50):
@@ -124,11 +170,13 @@ for x in range (50):
 for x in range (3):
     for y in range (3):
             maze[x,y]= 0
-for x in range (47, 50):
-    for y in range (47,50):
+for x in range (47, 49):
+    for y in range (47,49):
             maze[x,y]= 0
+wallHugger(maze)
 img = pyplot.imshow(maze,interpolation='nearest')
 #==============================================================================
 # aStarActor(maze, (0,0), (49,49))
 #==============================================================================
 pyplot.show()
+
